afeb880c014fd34a79e57d7cea7f44368cb3414c
Rahulgans <parurahul17@gmail.com>
2017-09-13 13:19:51 +0530

    basic assignment

--- angular/premier.js
+++ angular/premier.js
@@ -3,44 +3,37 @@
 var myApp = angular.module('matchesApp',['ngRoute']);
 
 myApp.controller("MainController", ['$http',function($http){
-
-	
+ 
+  var main = this;
 
 	this.title = "Stats for EPL";
 	this.baseUrl1 = "https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.json";
 	this.baseUrl2 = "https://raw.githubusercontent.com/openfootball/football.json/master/2016-17/en.1.json";
-	this.name;
-  this.year;
-	this.logo = "images/PL.jpg";
-	
-    var main= this ;
+	this.logo = "images/PL.jpg";   
     
-    this.nameFirst ;
-    this.nameSecond ;
-      this.matches1 = [];  
-     this.matches2 = [];
     // this.teams =[];
-     this.dates =[];
+    
      this.totalMatches = [];
      this.rounds1 = [];
      this.rounds2 = [];
-    this.days = [];
+
+   
     
      this.loadFirst = function(){
- 
+
       $http({
         method: 'GET',
         url:main.baseUrl1
       }).then(function successCallback(response) {
           // this callback will be called asynchronously
-          // when the response is available
+                    // when the response is available
+    
          main.rounds1 = response.data.rounds;
            main.nameFirst = response.data.name;       
-                  console.log(main.rounds1);
-    
-         
+          //        console.log(main.rounds1);
+
       
-        // console.log(main.dates.length); */
+        // console.log(main.dates.length); 
 
         }, function errorCallback(response) {
           // called asynchronously if an error occurs
@@ -48,13 +41,9 @@ myApp.controller("MainController", ['$http',function($http){
           alert("some error occurred. Check the console.");
 
         })
-
-    
-       //   console.log(main.dates);
-     
-      //console.log(main.matches1);
   };
 
+this.loadFirst() // calling loadFirst method
  
   this.loadSecond = function(){
 
@@ -68,9 +57,8 @@ myApp.controller("MainController", ['$http',function($http){
        main.nameSecond = response.data.name;
           main.rounds2 = response.data.rounds;
 
-
-          
-          
+      //    console.log(main.rounds1)          
+        //  main.matchStats1(response);
           
         }, function errorCallback(response) {
           // called asynchronously if an error occurs
@@ -79,13 +67,126 @@ myApp.controller("MainController", ['$http',function($http){
 
         })
      
-      //console.log(main.matches1);
+       //console.log(main.matches1);
   };
+  this.loadSecond() // calling LoadSecond method
 
-     } // controller function ends
 
-])  // controller ends
+}]) // controller ends   
 
+
+myApp.controller("MatchController", ['$http','$routeParams',function($http,$routeParams){
+
+
+
+var main = this ;
+
+
+  this.matchId1 = $routeParams.matchid1 ;
+  this.matchId2 = $routeParams.matchid2 ;
+  this.matchDate = $routeParams.matchdate ;
+    
+    this.team1;
+    this.team2;
+
+    this.score1;
+    
+    this.score2;
+    
+    this.code1;
+    
+    this.code2;
+    
+    this.winner;
+    this.date;
+    this.day;
+    this.rounds1 = [];
+
+this.baseUrl1 = "https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.json";
+  this.baseUrl2 = "https://raw.githubusercontent.com/openfootball/football.json/master/2016-17/en.1.json";
+
+  this.navigateBack = function (){
+    window.history.back();
+  }
+
+  this.matchStats = function(data){
+    console.log(data);
+    main.rounds1 = data.rounds;
+  //  console.log(main.rounds1);
+//  console.log(main.matchId1);
+ // console.log(main.matchId2);
+
+      for (var i in main.rounds1){
+
+
+         for (var j in main.rounds1[i].matches){
+
+            if (i== main.matchId1 && j == main.matchId2 && main.rounds1[i].matches[j].date == main.matchDate){
+
+          //    console.log("working");
+
+              main.day = main.rounds1[main.matchId1].name;
+                        
+                        main.date = main.rounds1[main.matchId1].matches[main.matchId2].date;
+                        main.team1 = main.rounds1[main.matchId1].matches[main.matchId2].team1.name;
+                     //   console.log(main.team1);
+                        main.team2 = main.rounds1[main.matchId1].matches[main.matchId2].team2.name;
+                     //   console.log(main.team2);
+
+                        main.score1 =main.rounds1[main.matchId1].matches[main.matchId2].score1;
+                        main.score2 = main.rounds1[main.matchId1].matches[main.matchId2].score2;
+                        main.code1 = main.rounds1[main.matchId1].matches[main.matchId2].team1.code;
+                    //    console.log(main.code1);
+                         main.code2 = main.rounds1[main.matchId1].matches[main.matchId2].team2.code;
+                    //     console.log(main.code2);
+
+                         if (main.score1 > main.score2){
+                            main.winner = ""+main.team1+" won" ; 
+                         }
+                         else if (main.score1 < main.score2){
+                          main.winner = ""+main.team2+" won" ;
+                         }
+                         else {
+                          main.winner = "Match drawn" ;
+                         } 
+                       }
+                     }
+                     }
+  } 
+
+    this.loadYear1 = function(){
+
+      $http({
+        method:"GET",
+        url:main.baseUrl1
+      }).then(function successCallback(response){
+
+        main.matchStats(response.data);
+      }, function errorCallback(reason){
+      alert("Error in GET");
+      })
+    }
+
+    this.loadYear1() // Calls loadYear1 method
+
+    this.loadYear2 = function(){
+
+      $http({
+        method:"GET",
+        url:main.baseUrl2
+      }).then(function successCallback(response){
+
+        main.matchStats(response.data);
+      }, function errorCallback(reason){
+      alert("Error in GET");
+      })
+    }
+
+     this.loadYear2();   // Calls loadYear2 method
+   }
+
+
+   ]); // controller ends
 
 
 
@@ -98,6 +199,12 @@ myApp.config(["$routeProvider",function($routeProvider){
     controller : "MainController",
     controllerAs : "mainCtrl"
   })
+ .when("/match/:matchid1/:matchid2/:matchdate",{
+    templateUrl : "views/matchDetail-view.html",
+    controller : "MatchController",
+    controllerAs : "matchCtrl"
+  }) 
+
   .otherwise(
             {
                 redirectTo:'/'
@@ -107,66 +214,4 @@ myApp.config(["$routeProvider",function($routeProvider){
 }]);
   
 
- /*   this.loadMatches = function(){
-
-
-      if (main.year == 2015){
-        
-      }
-    main.getFirst = function(){
-
-    	$http({
-        method: 'GET',
-        url:main.baseUrl1
-      }).then(function successCallback(response) {
-          // this callback will be called asynchronously
-          // when the response is available
-         	main.matches1.push(response.data);
-         	main.getSecond();
-         	
-         	//   console.log(this.matches1);
-         	
-        }, function errorCallback(response) {
-          // called asynchronously if an error occurs
-          // or server returns response with an error status.
-          alert("some error occurred. Check the console.");
-
-        })
-  };
-
-    this.getSecond = function(){
-
-     $http({
-        method: 'GET',
-        url: this.baseUrl2
-      }).then(function successCallback(response) {
-          // this callback will be called asynchronously
-          // when the response is available
-          this.matches2.push(response.data);
-          this.matches1.push(this.matches2[0]);
-
-          this.getTotal();
-
-        }, function errorCallback(response) {
-          // called asynchronously if an error occurs
-          // or server returns response with an error status.
-          alert("some error occurred. Check the console.");
-
-        })
-
-    } 
-   
-    this.getTotal = function(){
-		    
-		 this.totalMatches = this.matches1;
-
-
-    } 
-
-    this.getFirst() ; 
-
-
-  }; 
-   
-*/
 
