07211f364079b05ff6b27033bb497987ea7897c0
Rahulgans <parurahul17@gmail.com>
2017-09-17 18:40:25 +0530

    new

--- angular/controllers/statistics-controller.js
+++ angular/controllers/statistics-controller.js
@@ -183,14 +183,10 @@ myApp.controller("StatsController",["$http",function($http){
                     alert("Team didn't play in 2015 !");
                    // main.value1 = false;
                    }
-                 }
- 
-
-             , function errorCallback(reason){   
-                alert("Error in GET");
-                  })           
-
-              }
+                   }
+                 }, function errorCallback(reason){   
+                     alert("Error in GET");
+                  })          
                }; 
 
             this.statsCheck2 = function(data1){ 
